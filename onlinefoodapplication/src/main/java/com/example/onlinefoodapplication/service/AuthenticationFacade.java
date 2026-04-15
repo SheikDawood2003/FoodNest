1ad143c1556ff5831160b10e52bf4adee1bce9c7
SheikDawood2003 <139861582+SheikDawood2003@users.noreply.github.com>
2026-04-15 10:49:35 +0530

    Delete onlinefoodapplication directory

--- onlinefoodapplication/src/main/java/com/example/onlinefoodapplication/service/AuthenticationFacade.java
+++
@@ -1,8 +0,0 @@
-package com.example.onlinefoodapplication.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface AuthenticationFacade {
-
-    Authentication getAuthentication();
-}
